95c2f6ab987ba6730e6c79ed961722d4d36fe061
Tong Zhang <zhangt@frib.msu.edu>
2019-07-26 14:48:52 -0400

    MNT: Add DB_NAME, DB_USER, DB_PASS envs for database.
    - The default values are:
      - DB_NAME: phyapps_cloud
      - DB_USER: devuser
      - DB_PASS: 4e043a583

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import os
 
 DEBUG = True
-#SERVER_NAME = '127.0.0.1:5050'
+
+db_name = os.environ.get('DB_NAME', 'phyapps_cloud')
+db_user = os.environ.get('DB_USER', 'devuser')
+db_pass = os.environ.get('DB_PASS', '4e043a583')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -10,9 +15,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # MySQL
 SQLALCHEMY_DATABASE_URI = \
-   'mysql+pymysql://{username}:{password}@{host}/phyapps_cloud'.format(
-       username='devuser',
-       password='E=mc^2',
+   'mysql+pymysql://{db_user}:{db_pass}@{host}/{db_name}'.format(
+       db_user=db_user,
+       db_pass=db_pass,
+       db_name=db_name,
        host='localhost:3307')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
