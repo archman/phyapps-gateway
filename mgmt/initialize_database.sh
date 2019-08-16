11f4ae10e9da285c74e8cf8bcab37d3d447f9540
Tong Zhang <zhangt@frib.msu.edu>
2019-08-16 13:25:13 -0400

    REF: Update cmd 'initialize_database'.

--- mgmt/initialize_database.sh
+++ mgmt/initialize_database.sh
@@ -5,8 +5,13 @@
 # Only do once after phyapps-gateway is up for the first time.
 #
 
+d=$(dirname $FLASK_APP)/migrations
+[ -e $d ] && /bin/rm -rf $d
+
 flask db init && \
     flask db migrate -m "Initialize database" && \
-    flask db upgrade && \
-    config_admin_account.py
+    flask db upgrade
+
+# set up admin account:
+# config_admin_account.py
 
